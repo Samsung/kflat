10a37704337660eeaf6c661ad8926ae88d4ebd4b
Bartosz Zator <b.zator@samsung.com>
2023-02-05 11:19:45 +0100

    Added missing tests for flexible members

--- tests/example_flexible.c
+++ tests/example_flexible.c
@@ -1,7 +1,7 @@
 /**
  * @file example_flexible.c
  * @author Samsung R&D Poland - Mobile Security Group
- * 
+ *
  */
 
 #include "common.h"
@@ -9,6 +9,7 @@
 struct flex_B {
 	int field;
 };
+
 struct flex_A {
 	int get_obj_supported;
 	size_t cnt;
@@ -21,6 +22,24 @@ struct flex_C {
 	unsigned long arr[0];
 };
 
+typedef struct {
+	int field;
+} flex_D;
+
+struct flex_E {
+	size_t cnt;
+	flex_D arr[0];
+};
+
+union flex_F {
+	int field;
+};
+
+struct flex_G {
+	size_t cnt;
+	union flex_F arr[0];
+};
+
 #ifdef __KERNEL__
 
 FUNCTION_DEFINE_FLATTEN_STRUCT(flex_B);
@@ -31,6 +50,16 @@ FUNCTION_DEFINE_FLATTEN_STRUCT(flex_A,
 FUNCTION_DEFINE_FLATTEN_STRUCT(flex_C,
 	AGGREGATE_FLATTEN_STRING(name);
 	AGGREGATE_FLATTEN_TYPE_ARRAY_FLEXIBLE(unsigned long, arr);
+);
+
+FUNCTION_DEFINE_FLATTEN_STRUCT_TYPE(flex_D);
+FUNCTION_DEFINE_FLATTEN_STRUCT(flex_E,
+	AGGREGATE_FLATTEN_STRUCT_TYPE_FLEXIBLE(flex_D, arr);
+);
+
+FUNCTION_DEFINE_FLATTEN_UNION(flex_F);
+FUNCTION_DEFINE_FLATTEN_STRUCT(flex_G,
+	AGGREGATE_FLATTEN_UNION_FLEXIBLE(flex_F, arr);
 );
 
 static int kflat_flexible_test(struct kflat *kflat) {
@@ -48,6 +77,18 @@ static int kflat_flexible_test(struct kflat *kflat) {
 		c->arr[i] = ((c->l + 888*i) >> 4)% 16;
 	}
 
+	struct flex_E *e = kmalloc(sizeof(struct flex_E) + 3 * sizeof(flex_D), GFP_KERNEL);
+	e->cnt = 3;
+	e->arr[0].field = 2;
+	e->arr[1].field = 0xcceeff;
+	e->arr[2].field = 0xba5eba11;
+
+	struct flex_G *g = kmalloc(sizeof(struct flex_G) + 3 * sizeof(union flex_F), GFP_KERNEL);
+	g->cnt = 3;
+	g->arr[0].field = 3;
+	g->arr[1].field = 0xddeeaa;
+	g->arr[2].field = 0xf01dab1e;
+
 	FOR_ROOT_POINTER(a,
 		FLATTEN_STRUCT(flex_A, a);
     );
@@ -56,8 +97,18 @@ static int kflat_flexible_test(struct kflat *kflat) {
 		FLATTEN_STRUCT(flex_C, c);
     );
 
+    FOR_ROOT_POINTER(e,
+		FLATTEN_STRUCT(flex_E, e);
+    );
+
+    FOR_ROOT_POINTER(g,
+		FLATTEN_STRUCT(flex_G, g);
+    );
+
 	kfree(a);
 	kfree(c);
+	kfree(e);
+	kfree(g);
 	return 0;
 }
 
@@ -66,6 +117,8 @@ static int kflat_flexible_test(struct kflat *kflat) {
 static int kflat_flexible_validate(void *memory, size_t size, CFlatten flatten) {
 	struct flex_A *pA = (struct flex_A*)flatten_root_pointer_seq(flatten, 0);
 	struct flex_C *pC = (struct flex_C*)flatten_root_pointer_seq(flatten, 1);
+	struct flex_E *pE = (struct flex_E*)flatten_root_pointer_seq(flatten, 2);
+	struct flex_G *pG = (struct flex_G*)flatten_root_pointer_seq(flatten, 3);
 	
 	if(!pA->get_obj_supported)
 		return KFLAT_TEST_UNSUPPORTED;
@@ -80,6 +133,16 @@ static int kflat_flexible_validate(void *memory, size_t size, CFlatten flatten) 
 	for (int i=0; i<10; ++i) {
 		ASSERT_EQ(pC->arr[i],((pC->l + 888*i) >> 4)% 16);
 	}
+
+	ASSERT(pE->cnt == 3);
+	ASSERT(pE->arr[0].field == 2);
+	ASSERT(pE->arr[1].field == 0xcceeff);
+	ASSERT(pE->arr[2].field == 0xba5eba11);
+
+	ASSERT(pG->cnt == 3);
+	ASSERT(pG->arr[0].field == 3);
+	ASSERT(pG->arr[1].field == 0xddeeaa);
+	ASSERT(pG->arr[2].field == 0xf01dab1e);
 
 	return KFLAT_TEST_SUCCESS;
 }
---
+++ tests/example_flexible_self_contained.c
@@ -0,0 +1,126 @@
+/**
+ * @file example_flexible_self_contained.c
+ * @author Samsung R&D Poland - Mobile Security Group
+ *
+ */
+
+/*
+--AGGREGATE_FLATTEN_UNION_FLEXIBLE_SELF_CONTAINED
+*/
+
+#include "common.h"
+
+struct flexsc_B {
+	int field;
+};
+
+struct flexsc_A {
+	int get_obj_supported;
+	size_t cnt;
+	struct flexsc_B arr[0];
+};
+
+typedef struct {
+	int field;
+} flexsc_D;
+
+struct flexsc_E {
+	size_t cnt;
+	flexsc_D arr[0];
+};
+
+union flexsc_F {
+	int field;
+};
+
+struct flexsc_G {
+	size_t cnt;
+	union flexsc_F arr[0];
+};
+
+#ifdef __KERNEL__
+
+FUNCTION_DEFINE_FLATTEN_STRUCT(flexsc_B);
+FUNCTION_DEFINE_FLATTEN_STRUCT(flexsc_A,
+	AGGREGATE_FLATTEN_STRUCT_FLEXIBLE_SELF_CONTAINED(flexsc_B, sizeof(struct flexsc_B), offsetof(struct flexsc_A,arr));
+);
+
+FUNCTION_DEFINE_FLATTEN_STRUCT_TYPE(flexsc_D);
+FUNCTION_DEFINE_FLATTEN_STRUCT(flexsc_E,
+	AGGREGATE_FLATTEN_STRUCT_TYPE_FLEXIBLE_SELF_CONTAINED(flexsc_D, sizeof(flexsc_D), offsetof(struct flexsc_E,arr));
+);
+
+FUNCTION_DEFINE_FLATTEN_UNION(flexsc_F);
+FUNCTION_DEFINE_FLATTEN_STRUCT(flexsc_G,
+	AGGREGATE_FLATTEN_UNION_FLEXIBLE_SELF_CONTAINED(flexsc_F, sizeof(union flexsc_F), offsetof(struct flexsc_G,arr));
+);
+
+static int kflat_flexible_self_contained_test(struct kflat *kflat) {
+	struct flexsc_A *a = kmalloc(sizeof(struct flexsc_A) + 3 * sizeof(struct flexsc_B), GFP_KERNEL);
+	a->get_obj_supported = IS_ENABLED(KFLAT_GET_OBJ_SUPPORT);
+	a->cnt = 3;
+	a->arr[0].field = 1;
+	a->arr[1].field = 0xaaddcc;
+	a->arr[2].field = 0xcafecafe;
+
+	struct flexsc_E *e = kmalloc(sizeof(struct flexsc_E) + 3 * sizeof(flexsc_D), GFP_KERNEL);
+	e->cnt = 3;
+	e->arr[0].field = 2;
+	e->arr[1].field = 0xcceeff;
+	e->arr[2].field = 0xba5eba11;
+
+	struct flexsc_G *g = kmalloc(sizeof(struct flexsc_G) + 3 * sizeof(union flexsc_F), GFP_KERNEL);
+	g->cnt = 3;
+	g->arr[0].field = 3;
+	g->arr[1].field = 0xddeeaa;
+	g->arr[2].field = 0xf01dab1e;
+
+	FOR_ROOT_POINTER(a,
+		FLATTEN_STRUCT(flexsc_A, a);
+    );
+
+    FOR_ROOT_POINTER(e,
+		FLATTEN_STRUCT(flexsc_E, e);
+    );
+
+    FOR_ROOT_POINTER(g,
+		FLATTEN_STRUCT(flexsc_G, g);
+    );
+
+	kfree(a);
+	kfree(e);
+	kfree(g);
+	return 0;
+}
+
+#else
+
+static int kflat_flexible_self_contained_validate(void *memory, size_t size, CFlatten flatten) {
+	struct flexsc_A *pA = (struct flexsc_A*)flatten_root_pointer_seq(flatten, 0);
+	struct flexsc_E *pE = (struct flexsc_E*)flatten_root_pointer_seq(flatten, 1);
+	struct flexsc_G *pG = (struct flexsc_G*)flatten_root_pointer_seq(flatten, 2);
+	
+	if(!pA->get_obj_supported)
+		return KFLAT_TEST_UNSUPPORTED;
+
+	ASSERT(pA->cnt == 3);
+	ASSERT(pA->arr[0].field == 1);
+	ASSERT(pA->arr[1].field == 0xaaddcc);
+	ASSERT(pA->arr[2].field == 0xcafecafe);
+
+	ASSERT(pE->cnt == 3);
+	ASSERT(pE->arr[0].field == 2);
+	ASSERT(pE->arr[1].field == 0xcceeff);
+	ASSERT(pE->arr[2].field == 0xba5eba11);
+
+	ASSERT(pG->cnt == 3);
+	ASSERT(pG->arr[0].field == 3);
+	ASSERT(pG->arr[1].field == 0xddeeaa);
+	ASSERT(pG->arr[2].field == 0xf01dab1e);
+
+	return KFLAT_TEST_SUCCESS;
+}
+
+#endif
+
+KFLAT_REGISTER_TEST("FLEXIBLE_SELF_CONTAINED", kflat_flexible_self_contained_test, kflat_flexible_self_contained_validate);
