5d7ca3ef9612b6cee03327676538f83155029778
Bartosz Zator <b.zator@samsung.com>
2023-04-25 23:44:54 +0200

    Added large interval tree test

---
+++ tests/example_large_interval_tree.c
@@ -0,0 +1,112 @@
+/**
+ * @file example_large_interval_tree.c
+ * @author Samsung R&D Poland - Mobile Security Group
+ * 
+ */
+
+#include "common.h"
+
+#define INTERVAL_COUNT	5000
+
+#ifdef __KERNEL__
+#include <linux/rbtree.h>
+#include <linux/random.h>
+#include <linux/interval_tree_generic.h>
+#else
+#include <stdbool.h>
+#include "../lib/include_priv/rbtree.h"
+#endif
+
+struct my_interval_tree_node {
+    struct rb_node rb;
+    uint64_t __subtree_last;
+
+    uint64_t start;
+    uint64_t end;
+    uint64_t phys_addr;
+};
+
+struct my_interval_tree_map {
+    struct rb_root_cached imap_root;
+};
+
+size_t my_interval_tree_count(const struct my_interval_tree_map* interval_tree_map) {
+    size_t count = 0;
+    const struct rb_root* root;
+    struct rb_node* p;
+
+    root = &interval_tree_map->imap_root.rb_root;
+    for(p = rb_first_postorder(root); p != NULL; p = rb_next_postorder(p)) {
+        count++;
+    }
+    return count;
+}
+
+#ifdef __KERNEL__
+
+static struct my_interval_tree_map interval_tree_map;
+
+/*******************************************************
+ * INTERVAL TREE
+ *******************************************************/
+#define START(node) ((node)->start)
+#define END(node)  ((node)->end)
+
+INTERVAL_TREE_DEFINE(struct my_interval_tree_node, rb,
+		     uintptr_t, __subtree_last,
+		     START, END,
+             static __attribute__((used)), my_interval_tree)
+
+FUNCTION_DECLARE_FLATTEN_STRUCT(my_interval_tree_node);
+FUNCTION_DEFINE_FLATTEN_STRUCT(my_interval_tree_node,
+	AGGREGATE_FLATTEN_STRUCT_EMBEDDED_POINTER(my_interval_tree_node, rb.__rb_parent_color, ptr_clear_2lsb_bits, flatten_ptr_restore_2lsb_bits);
+    AGGREGATE_FLATTEN_STRUCT(my_interval_tree_node, rb.rb_right);
+    AGGREGATE_FLATTEN_STRUCT(my_interval_tree_node, rb.rb_left);
+);
+
+FUNCTION_DEFINE_FLATTEN_STRUCT(my_interval_tree_map,
+    AGGREGATE_FLATTEN_STRUCT(my_interval_tree_node, imap_root.rb_root.rb_node);
+    AGGREGATE_FLATTEN_STRUCT(my_interval_tree_node, imap_root.rb_leftmost);
+);
+
+static int kflat_large_interval_tree_test(struct kflat *kflat) {
+	
+	unsigned j;
+	unsigned long i=0;
+	unsigned char *bytes = kflat_zalloc(kflat,INTERVAL_COUNT*10,1);
+	get_random_bytes(bytes, INTERVAL_COUNT*10);
+	interval_tree_map.imap_root.rb_root = RB_ROOT;
+
+	for (j = 0; j < INTERVAL_COUNT; ++j) {
+		struct my_interval_tree_node* node = kflat_zalloc(kflat,sizeof(*node),1);
+	    node->start = i+bytes[j*10];
+	    node->end = node->start+bytes[j*10+1]-1;
+	    node->phys_addr = *((uint64_t*)&bytes[j*10+2]);;
+	    my_interval_tree_insert(node, &interval_tree_map.imap_root);
+	}
+
+	flat_infos("Created %zu intervals in the interval tree",my_interval_tree_count(&interval_tree_map));
+
+	FOR_ROOT_POINTER(&interval_tree_map,
+		FLATTEN_STRUCT(my_interval_tree_map, &interval_tree_map);
+	);
+
+	interval_tree_map.imap_root.rb_root = RB_ROOT;
+	return 0;
+}
+
+#else
+
+static int kflat_large_interval_tree_validate(void *memory, size_t size, CFlatten flatten) {
+	const struct my_interval_tree_map *interval_tree_map = (struct my_interval_tree_map *)memory;
+	size_t count = my_interval_tree_count(interval_tree_map);
+	ASSERT(count == INTERVAL_COUNT);
+
+	PRINT("Interval tree size: %ld", count);
+
+	return KFLAT_TEST_SUCCESS;
+}
+
+#endif
+
+KFLAT_REGISTER_TEST_FLAGS("LARGE_INTERVAL_TREE", kflat_large_interval_tree_test, kflat_large_interval_tree_validate,KFLAT_TEST_ATOMIC);
