8d674ff5283d79788d31aca174945638b54f435c
Pawel Wieczorek <p.wieczorek@samsung.com>
2024-08-20 15:27:17 +0200

    Output stripped library version directly to libunflatten.a file

--- utils/unflatten_for_aot.sh
+++ utils/unflatten_for_aot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Utility script that marks all rb_tree-related functions in libunflatten as local 
 # in order to avoid linking issues (when running KFLAT + AOT).
-# Outputs libunflatten_aot.a
+# Outputs libunflatten.a
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <path_to_cmake_build_dir>"
@@ -12,7 +12,7 @@ BINARY_DIR=$1
 UNFLATTEN_O_PATH=$BINARY_DIR/lib/CMakeFiles/unflatten_obj.dir/unflatten.cpp.o
 RBTREE_O_PATH=$BINARY_DIR/lib/CMakeFiles/unflatten_obj.dir/rbtree.c.o
 OUT_O=$BINARY_DIR/lib/CMakeFiles/unflatten_obj.dir/unflatten_combined.o
-UNFLATTEN_LIB_OUT=$BINARY_DIR/lib/libunflatten_aot.a
+UNFLATTEN_LIB_OUT=$BINARY_DIR/lib/libunflatten.a
 
 if [ ! -f $UNFLATTEN_O_PATH ] || [ ! -f $RBTREE_O_PATH ]; then
     echo "You need to build kflat's \"unflatten\" target first."
@@ -29,4 +29,5 @@ for sym in ${SYMS_TO_LOCALIZE[@]}; do
     objcopy --localize-symbol=$sym $OUT_O
 done
 
+rm -f $UNFLATTEN_LIB_OUT
 ar -rcs $UNFLATTEN_LIB_OUT $OUT_O 
